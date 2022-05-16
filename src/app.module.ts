8b907efb2acd1329903b9e42f034866f77b1c767
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-05-16 15:57:50 +0600

    Add course module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [],
+  imports: [CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
