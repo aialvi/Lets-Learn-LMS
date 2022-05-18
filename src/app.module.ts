1c3b09bac82670a5b8cfd2188c5fb4850c5514e2
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-05-18 14:31:43 +0600

    Add auth module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
