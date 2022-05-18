0a58b42c77e159880a9595bb30772468cab9a692
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-05-18 16:39:43 +0600

    Add auth service in app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 
   @Get()
