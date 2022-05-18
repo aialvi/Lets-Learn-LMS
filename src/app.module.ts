a5bce26565cb90714b14ee56e984dde7f920cd75
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-05-18 14:32:34 +0600

    Add user module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CoursesModule, AuthModule],
+  imports: [CoursesModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
